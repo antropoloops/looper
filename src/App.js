1e1e7d2bc565a49897c8bacba8a3189f96dba017
danigb <danigb@gmail.com>
2017-04-05 19:39:15 +0200

    Poor's man redux

--- src/App.js
+++ src/App.js
@@ -1,37 +1,47 @@
 import React, { Component } from "react";
-import classNames from "classnames"
 import KeyHandler from "./KeyHandler"
+import Pads from "./Pads"
 import "./App.css";
 
 const LETTERS = "QWEASDZXC".split("")
-const Pad = ({ letter, pressed }) => (
-  <div className={classNames("Pad", { pressed })}>{letter} {pressed}</div>
-)
 
-const Pads = ({ letters, pressed }) => (
-  <div className="Pads">
-    {letters.map(letter => (
-      <Pad key={letter} letter={letter} pressed={pressed.indexOf(letter) !== -1} />
-    ))}
-  </div>
-)
+// ## Poor's man redux: reducer + dispatch + logger middleware
 
+// Reduce: given a state and an action, return a new state
+const reduce = (state, action = {}) => {
+  switch (action.type) {
+    case "KEYUP":
+      const pressed = state.pressed.filter(k => k !== action.key)
+      return { pressed }
+    case "KEYDOWN":
+      state.pressed.push(action.key)
+      return state
+    default:
+      return state
+  }
+}
+
+// Poor's man logger middleware
+const log = (fn, enabled) => (state, action) => {
+  if (enabled) console.log("ACTION", state, action)
+  state = fn(state, action)
+  if (enabled) console.log("STATE", state)
+  return state
+}
+
+// The reducer after applying the middleware
+const reducer = log(reduce, false)
+
+const initialState = { pressed: [] }
 class App extends Component {
-  constructor (props) {
-    super(props)
-    this.state = { pressed: [] }
-    this.keyDown = this.keyDown.bind(this)
-    this.keyUp = this.keyUp.bind(this)
+  // set the initial state
+  state = reducer(initialState)
+
+  dispatch (action) {
+    this.setState(prevState => reducer(prevState, action))
   }
-  keyDown (key) {
-    const pressed = this.state.pressed
-    pressed.push(key)
-    this.setState({ pressed })
-  }
-  keyUp (key) {
-    const pressed = this.state.pressed.filter(e => e !== key)
-    this.setState({ pressed })
-  }
+  keyDown = (key) => this.dispatch({ type: "KEYDOWN", key })
+  keyUp = (key) => this.dispatch({ type: "KEYUP", key })
 
   render() {
     return (
